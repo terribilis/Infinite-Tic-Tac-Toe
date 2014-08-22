15628d9b3b47c99e3f7da2b7f3a5b62351fcc633
terribilis <ted.kanell@gmail.com>
2014-08-21 21:45:01 -0600

    Update script.js
    Created Functions to make an X and a Why

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("is this working");
+function show_image(src, width, height, alt) {
+  var img = document.createElement("img");
+  img.src = src;
+  img.width = width;
+  img.height = height;
+  img.alt = alt;
+
+  // This next line will just add it to the <body> tag
+  document.body.appendChild(img); 
+};
+function add_X(){
+  var src = "http://bookriotcom.c.presscdn.com/wp-content/uploads/2013/07/x.png";
+  show_image('http://bookriotcom.c.presscdn.com/wp-content/uploads/2013/07/x.png', 50, 50, "X")
+};
+function add_O(){
+  var src = "http://store.hamiltonmarine.com/prodimg/BER-O.JPG";
+  show_image('http://store.hamiltonmarine.com/prodimg/BER-O.JPG', 50, 50, "x")
+};
+
