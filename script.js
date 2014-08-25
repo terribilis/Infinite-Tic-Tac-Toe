a252c6c40a36c490e425fbd2e167da380070156f
terribilis <ted.kanell@gmail.com>
2014-08-24 22:18:12 -0600

    Update script.js

--- script.js
+++ script.js
@@ -4,18 +4,28 @@ function show_image(sender, src, width, height, alt) {
   img.width = width;
   img.height = height;
   img.alt = alt;
-  //document.body.appendChild(img);
-  // This next line will just add it to the <body> tag
+
+
+  //document.body.appendChild(img); //DO NOT DO THIS.
+
+  //REPLACE WITH BELLOW CODE
   var TD = $("<td></td>");
   TD.append(img);
 
-  $(sender).parent().append(TD); 
+  $(sender).parent().append(TD); //ur element will be appended in the same row, but separated <td></td>.
+
+  //or u use this to append in the same <td></td>
+
+   $(sender).append(img);
+
 };
+
 function add_X(){
   var src = "http://bookriotcom.c.presscdn.com/wp-content/uploads/2013/07/x.png";
   show_image($(this),'http://bookriotcom.c.presscdn.com/wp-content/uploads/2013/07/x.png', 60, 60, "X")
 };
+
 function add_O(){
   var src = "http://store.hamiltonmarine.com/prodimg/BER-O.JPG";
-  show_image($(this), 'http://store.hamiltonmarine.com/prodimg/BER-O.JPG', 50, 50, "x")
+  show_image($(this),'http://store.hamiltonmarine.com/prodimg/BER-O.JPG', 50, 50, "x")
 };
