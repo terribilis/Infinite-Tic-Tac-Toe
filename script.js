758606bb53019a505837e223047648ce5593c676
terribilis <ted.kanell@gmail.com>
2014-08-24 21:58:59 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function show_image(src, width, height, alt) {
+function show_image(sender, src, width, height, alt) {
   var img = document.createElement("img");
   img.src = src;
   img.width = width;
@@ -13,9 +13,9 @@ function show_image(src, width, height, alt) {
 };
 function add_X(){
   var src = "http://bookriotcom.c.presscdn.com/wp-content/uploads/2013/07/x.png";
-  show_image('http://bookriotcom.c.presscdn.com/wp-content/uploads/2013/07/x.png', 60, 60, "X")
+  show_image($(this),'http://bookriotcom.c.presscdn.com/wp-content/uploads/2013/07/x.png', 60, 60, "X")
 };
 function add_O(){
   var src = "http://store.hamiltonmarine.com/prodimg/BER-O.JPG";
-  show_image('http://store.hamiltonmarine.com/prodimg/BER-O.JPG', 50, 50, "x")
+  show_image($(this), 'http://store.hamiltonmarine.com/prodimg/BER-O.JPG', 50, 50, "x")
 };
