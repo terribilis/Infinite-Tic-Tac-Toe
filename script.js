73397c0e39dba498c7be1fb9835062f44d49e26b
terribilis <ted.kanell@gmail.com>
2014-08-24 20:56:15 -0600

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function show_image(src, width, height, alt) {
   img.alt = alt;
 
   // This next line will just add it to the <body> tag
-  this.appendChild(img); 
+  document.this.appendChild(img); 
 };
 function add_X(){
   var src = "http://bookriotcom.c.presscdn.com/wp-content/uploads/2013/07/x.png";
